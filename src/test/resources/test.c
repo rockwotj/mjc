be6e7430b57f95e430e349e4cf37fe203bf3c565
Tyler Rockwood <rockwotj@rose-hulman.edu>
2016-05-06 12:22:19 -0400

    Committing test c file with virtual table

--- src/test/resources/test.c
+++ src/test/resources/test.c
@@ -1,35 +1,26 @@
+#include <stdlib.h>
 #include <stdio.h>
-//
-//typedef struct {
-//   int   foo;
-//} Test;
-//
-//int Test_init(Test* this) {
-//    (*this).foo = 3;
-//    return 0;
-//}
-//
-//int Test_test(int n, Test* this) {
-//    return n + (*this).foo;
-//}
-//
-//int main() {
-//    Test t;
-//    Test_init(&t);
-//    printf("%d\n", Test_test(2, &t));
-//    return 0;
-//}
+
+typedef struct Test Test;
+int Test_test(int n, Test* this);
+
+struct Test_methods {
+    int (*Test_test)(int,Test*);
+} Test_methods = {&Test_test};
+
+struct Test {
+   int   foo;
+   struct Test_methods* methods;
+};
+
+int Test_test(int n, Test* this) {
+    return n + this->foo;
+}
 
 int main() {
-       int i = 0;
-      int j = 1;
-test:
-    i += 1;
-    if (i > 3) {
-        goto exit;
-    }
-    goto test;
-exit:
-    printf("%d\n", i);
+    Test* t = malloc(sizeof(Test));
+    t->foo = 3;
+    t->methods = &Test_methods;
+    printf("%d\n", (*(t->methods->Test_test))(2, t));
     return 0;
 }
