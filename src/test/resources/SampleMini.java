84e87e8d46d918588704cf09704726261169e595
Tyler Rockwood <rockwotj@rose-hulman.edu>
2016-05-16 15:56:30 -0400

    Pushing code for David to see

--- src/test/resources/SampleMini.java
+++ src/test/resources/SampleMini.java
@@ -1,17 +1,23 @@
 class Main {
     public static void main(String[] args) {
-        System.out.print('H');
-        System.out.print('e');
-        System.out.print('l');
-        System.out.print('l');
-        System.out.print('o');
-        System.out.print(' ');
-        System.out.print('W');
-        System.out.print('o');
-        System.out.print('r');
-        System.out.print('l');
-        System.out.print('d');
-        System.out.print('!');
-        System.out.print('\n');
+        Foo f = new Foo();
+        int[] a = new int[4];
+        System.out.println(f.getMeaningOfLife());
+        int i = 0;
+        while (i < 4) {
+            System.out.println(a[i]);
+            i = i + 1;
+        }
     }
+}
+
+class Foo {
+
+    int life;
+
+    public int getMeaningOfLife() {
+        life = 42;
+        return life;
+    }
+
 }
